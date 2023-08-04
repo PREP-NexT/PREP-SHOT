c9b7f188136bfa2510ef2c33131e7fd9eb81debd
LIU ZHANWEI <50036800+Zhanwei-Liu@users.noreply.github.com>
2023-08-04 19:48:37 +0800

    Update load_data.py
    fix a bug to ignore static sheet when not including hydro

--- prepshot/load_data.py
+++ prepshot/load_data.py
@@ -87,7 +87,8 @@ def get_attr(para):
         None
     """
     para["year"] = sorted(list(para["discount_factor"].keys()))
-    para["stcd"] = list({i[1] for i in para["static"].keys()})
+    if "static" in para.keys():
+        para["stcd"] = list({i[1] for i in para["static"].keys()})
     para["hour"] = sorted({i[3] for i in para["demand"].keys() if isinstance(i[3], int)})
     para["month"] = sorted({i[2] for i in para["demand"].keys() if isinstance(i[2], int)})
     para["zone"] = list({i[0] for i in para["demand"].keys()})
