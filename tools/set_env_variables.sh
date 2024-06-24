72c8a38af93d8ce3010e1a77499df9d368f185d6
LIU ZHANWEI <50036800+Zhanwei-Liu@users.noreply.github.com>
2024-06-24 16:30:49 +0800

    add scripts to help pre-setting and post-analysis

---
+++ tools/set_env_variables.sh
@@ -0,0 +1,6 @@
+# highs
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/opt/highs/lib/
+# mosek
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:Users/energy/mosek/10.2/tools/platform/osxaarch64/bin/
+# copt
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/Users/energy/copt/copt71/lib/
